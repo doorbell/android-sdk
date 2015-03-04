8295a2701ba8e26df5ec1e34c9f862054b6e171d
Phil Manavopoulos <manavo@gmail.com>
2015-03-04 12:31:43 +0000

    Better localisation

--- library/src/io/doorbell/android/DoorbellApi.java
+++ library/src/io/doorbell/android/DoorbellApi.java
@@ -9,53 +9,57 @@ import android.app.Activity;
 
 
 public class DoorbellApi extends RestApi {
-	
-	private String mApiKey;
-	private long mAppId;
-	
-	public DoorbellApi(Activity activity) {
-		super(activity);
-		
-		this.BASE_URL = "https://doorbell.io/api/";
-		this.rest.setHost("doorbell.io");
-		this.setUserAgent("Doorbell Android SDK");
-		
-		this.acceptAllSslCertificates();
-	}
-	
-	public void setAppId(long id) {
-		this.mAppId = id;
-	}
-	
-	public void setApiKey(String key) {
-		this.mApiKey = key;
-	}
-	
-	public void reset() {
-		super.reset();
-		
-		this.cachePolicy = RestCache.CachePolicy.NETWORK_ONLY;
-	}
-	
-	public void impression() {
-		this.setLoadingMessage(null);
-		this.post("applications/"+this.mAppId+"/impression?key="+this.mApiKey);
-	}
-	
-	public void open() {
-		this.setLoadingMessage(null);
-		this.post("applications/"+this.mAppId+"/open?key="+this.mApiKey);
-	}
-	
-	public void sendFeedback(String message, String email, JSONObject properties, String name) {
-		this.addParameter("message", message);
-		this.addParameter("email", email);
-		
-		this.addParameter("properties", properties.toString());
-		
-		this.addParameter("name", name);
-		
-		this.post("applications/"+this.mAppId+"/submit?key="+this.mApiKey);
-	}
-	
+
+    private static final String DOORBELL_IO_URL = "https://doorbell.io/api/";
+    private static final String DOORBELL_IO_HOST = "doorbell.io";
+    private static final String DOORBELL_USER_AGENT = "Doorbell Android SDK";
+
+    private String mApiKey;
+    private long mAppId;
+
+    public DoorbellApi(Activity activity) {
+        super(activity);
+
+        this.BASE_URL = DOORBELL_IO_URL;
+        this.rest.setHost(DOORBELL_IO_HOST);
+        this.setUserAgent(DOORBELL_USER_AGENT);
+
+        this.acceptAllSslCertificates();
+    }
+
+    public void setAppId(long id) {
+        this.mAppId = id;
+    }
+
+    public void setApiKey(String key) {
+        this.mApiKey = key;
+    }
+
+    public void reset() {
+        super.reset();
+
+        this.cachePolicy = RestCache.CachePolicy.NETWORK_ONLY;
+    }
+
+    public void impression() {
+        this.setLoadingMessage(null);
+        this.post("applications/" + this.mAppId + "/impression?key=" + this.mApiKey);
+    }
+
+    public void open() {
+        this.setLoadingMessage(null);
+        this.post("applications/" + this.mAppId + "/open?key=" + this.mApiKey);
+    }
+
+    public void sendFeedback(String message, String email, JSONObject properties, String name) {
+        this.addParameter("message", message);
+        this.addParameter("email", email);
+
+        this.addParameter("properties", properties.toString());
+
+        this.addParameter("name", name);
+
+        this.post("applications/" + this.mAppId + "/submit?key=" + this.mApiKey);
+    }
+
 }
