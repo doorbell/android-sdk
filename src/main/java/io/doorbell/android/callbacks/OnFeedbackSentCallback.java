dc9ff5f10b6ff7659a794e03b775083944cf3b49
Jared Burrows <jaredsburrows@gmail.com>
2016-06-02 23:25:07 -0700

    Fix various ide warnings and tabs to spaces

--- src/main/java/io/doorbell/android/callbacks/OnFeedbackSentCallback.java
+++ src/main/java/io/doorbell/android/callbacks/OnFeedbackSentCallback.java
@@ -1,5 +1,5 @@
 package io.doorbell.android.callbacks;
 
 public interface OnFeedbackSentCallback {
-	public void handle(String message);
+    void handle(String message);
 }
--- src/main/java/io/doorbell/android/manavo/rest/RestErrorCallback.java
+++ src/main/java/io/doorbell/android/manavo/rest/RestErrorCallback.java
@@ -2,5 +2,5 @@ package io.doorbell.android.manavo.rest;
 
 
 public interface RestErrorCallback {
-	public void error(String message);
+    void error(String message);
 }
